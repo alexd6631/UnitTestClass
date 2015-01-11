cc8c23c6a0eb4d727a09338a5e9024ac656ec82a
Alexandre <alexd6631@gmail.com>
2015-01-11 17:27:19 +0100

    Division par zéro

--- test/com/ynov/test/CalculatorImplTest.java
+++ test/com/ynov/test/CalculatorImplTest.java
@@ -32,5 +32,15 @@ public class CalculatorImplTest {
         Calculator calculator = new CalculatorImpl();
         assertEquals(2, calculator.divide(10, 5));
         assertEquals(-1, calculator.divide(10, -10));
+        try {
+            calculator.divide(10, 0);
+            fail("Divide doit renvoyer ArithmeticException lorsque le dénominateur est égal à zéro");
+        } catch (ArithmeticException ignored) {  }
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() throws Exception {
+        Calculator calculator = new CalculatorImpl();
+        calculator.divide(10, 0);
     }
 }
