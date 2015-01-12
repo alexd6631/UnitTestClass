0ba0e0be691d2c613307d72ae1cbdd8a67cc9cd5
Alexandre <alexd6631@gmail.com>
2015-01-12 09:58:29 +0100

    Utilisation de @Before pour l'initialisation

--- test/com/ynov/test/CalculatorImplTest.java
+++ test/com/ynov/test/CalculatorImplTest.java
@@ -1,35 +1,38 @@
 package com.ynov.test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorImplTest {
+    private Calculator calculator;
+
+    @Before
+    public void initCalculator() {
+        calculator = new CalculatorImpl();
+    }
 
     @Test
     public void testAdd() throws Exception {
-        Calculator calculator = new CalculatorImpl();
         assertEquals(15, calculator.add(10, 5));
         assertEquals(0, calculator.add(10, -10));
     }
 
     @Test
     public void testSubstract() throws Exception {
-        Calculator calculator = new CalculatorImpl();
         assertEquals(5, calculator.substract(10, 5));
         assertEquals(20, calculator.substract(10, -10));
     }
 
     @Test
     public void testMultiply() throws Exception {
-        Calculator calculator = new CalculatorImpl();
         assertEquals(50, calculator.multiply(10, 5));
         assertEquals(-100, calculator.multiply(10, -10));
     }
 
     @Test
     public void testDivide() throws Exception {
-        Calculator calculator = new CalculatorImpl();
         assertEquals(2, calculator.divide(10, 5));
         assertEquals(-1, calculator.divide(10, -10));
         try {
@@ -40,7 +43,6 @@ public class CalculatorImplTest {
 
     @Test(expected = ArithmeticException.class)
     public void testDivideByZero() throws Exception {
-        Calculator calculator = new CalculatorImpl();
         calculator.divide(10, 0);
     }
 }
